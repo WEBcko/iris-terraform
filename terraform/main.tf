3083deb56929477cac03be83a3e563d767840e75
Erick Ojeda <57963965+ErickOjeda@users.noreply.github.com>
2025-06-15 19:01:31 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,8 @@ resource "google_container_cluster" "primary" {
 
   node_config {
     machine_type = var.machine_type
+    disk_type = "pd-standard"
+    disk_size_gb = 50
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
