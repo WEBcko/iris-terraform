b0c27c5e0bceff03f2e1117655729a7bfcb542d6
Roberto Vinicius Passos <31169609+RvPassos@users.noreply.github.com>
2025-06-16 19:49:57 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,8 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
+  deletion_protection = false
+ 
   initial_node_count = var.cluster_size
 
   node_config {
