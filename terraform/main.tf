7d130d8c8f41a6cd9c23ba429015faca5419263e
Roberto Vinicius Passos <31169609+RvPassos@users.noreply.github.com>
2025-06-16 19:53:20 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,8 +23,6 @@ provider "helm" {
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
-
-  deletion_protection = false
  
   initial_node_count = var.cluster_size
 
