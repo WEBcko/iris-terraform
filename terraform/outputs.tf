2aa9934ff95d89c9b88ecaf0333eae0e9ac22003
Roberto Vinicius Passos <31169609+RvPassos@users.noreply.github.com>
2025-06-17 04:26:38 -0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,7 @@ output "cluster_endpoint" {
   description = "Endpoint do cluster GKE"
   value       = google_container_cluster.primary.endpoint
 }
+
+output "ingress_ip_address" {
+  value = data.google_compute_address.ingress_ip.address
+}
